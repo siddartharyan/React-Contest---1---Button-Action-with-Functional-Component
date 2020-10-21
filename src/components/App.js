571eafcdd21e16a91257485b81b3865c079c7589
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-10-21 20:33:52 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [prev, setprev] = useState(false);
+  const handleClick = () => {
+    setprev(!prev);
+  };
+  let ans =
+    "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        Click here
+      </button>
+      {prev ? <p id="para">{ans}</p> : null}
     </div>
   );
 }
-
 
 export default App;
